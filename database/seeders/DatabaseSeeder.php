5efcba1c4475a42cc5709814876da35b9e8131db
Atakhan <kadyr.atakhanov@gmail.com>
2022-09-17 02:34:16 +0300

    make seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('authors')->insert(
+            [
+                ["name" => "Anton Chekhov",],
+                ["name" => "Joseph Conrad",],
+                ["name" => "Charles Dickens",],
+                ["name" => "Denis Diderot",],
+            ]
+        );
+        DB::table('books')->insert(
+            [
+                [
+                    "author_id" => 1,
+                    "cover" => "https://raw.githubusercontent.com/benoitvallon/100-best-books/master/static/images/stories-of-anton-chekhov.jpg",
+                    "title" => "Stories"
+                ],
+                [
+                    "author_id" => 2,
+                    "cover" => "https://raw.githubusercontent.com/benoitvallon/100-best-books/master/static/images/nostromo.jpg",
+                    "title" => "Nostromo"
+                ],
+                [
+                    "author_id" => 3,
+                    "cover" => "https://raw.githubusercontent.com/benoitvallon/100-best-books/master/static/images/great-expectations.jpg",
+                    "title" => "Great Expectations"
+                ],
+                [
+                    "author_id" => 4,
+                    "cover" => "https://raw.githubusercontent.com/benoitvallon/100-best-books/master/static/images/jacques-the-fatalist.jpg",
+                    "title" => "Jacques the Fatalist"
+                ]
+            ]
+        );
     }
 }
