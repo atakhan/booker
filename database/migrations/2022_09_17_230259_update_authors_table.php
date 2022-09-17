e893632fb1de2df58ae96ce9310b985e48f867ac
Atakhan <kadyr.atakhanov@gmail.com>
2022-09-18 02:17:50 +0300

    make author name field unique

---
+++ database/migrations/2022_09_17_230259_update_authors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('authors', function (Blueprint $table) {
+            $table->string('name', 128)->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
